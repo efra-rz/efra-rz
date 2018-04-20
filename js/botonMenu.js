256b402e9deece62ed69ab4f4e2fddb4b4b9caa1
Efra Rz <efrain.rmz1985@gmail.com>
2018-04-20 16:20:45 -0500

    Se actualizo menu para moviles

---
+++ js/botonMenu.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById('botonMenu');
+var menu = document.getElementById('menuNav');
+
+btn.addEventListener('click', function () {
+    menu.classList.toggle('mover');
+});
