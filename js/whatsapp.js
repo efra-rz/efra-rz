f126cbbd3d95f10423ff863a66b12e54d6a43802
efra-rz <efrain.rmz1985@gmail.com>
2020-10-24 21:36:23 -0500

    phone number correction

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   var gcaMain = new GetChatApp({
-    'mobileNumber' : '52181846022228',
+    'mobileNumber' : '5281846022228',
     'titleMessage' : '👋 Hola que tal!',
     'welcomeMessage': 'Hola que tal!🙌<br /> <br /> Si tienes alguna duda acerca de tu próximo sitio web no dudes en contactarme 🔥<br /> <br /> ~Efra',
     'position' : 'right'
